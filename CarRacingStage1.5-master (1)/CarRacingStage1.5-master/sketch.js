b30d023d9e7308a73ae62bcdcaa003064aa6dd98
ismaeelbiyabani <72075716+ismaeelbiyabani@users.noreply.github.com>
2020-11-20 15:32:04 +0530

    Add files via upload

---
+++ CarRacingStage1.5-master (1)/CarRacingStage1.5-master/sketch.js
@@ -0,0 +1,43 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2;
+var track,car1IMG,car2IMG,car3IMG,car4IMG;
+
+function preload(){
+  car1IMG=loadImage("car1.png");
+  car2IMG=loadImage("car2.png");
+
+
+  track=loadImage("track.jpg");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 2){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState===2){
+    game.end();
+    alert("Your rank:"+player.rank);
+  }
+}
